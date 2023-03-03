c89f072b7d40b6cdb36f763fd62a810f25fa7f9b
Richard Race <1273965+richrace@users.noreply.github.com>
2023-03-03 14:49:23 +0000

    Use correct key for react

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -13,7 +13,7 @@ function Slider({ skill, index, descriptors }) {
       { value: 5, text: descriptors[4] },
     ],
     layout: "stacked",
-    style: { "align-items": "center" },
+    style: { alignItems: "center" },
   };
   return (
     <RadarConsumer>
